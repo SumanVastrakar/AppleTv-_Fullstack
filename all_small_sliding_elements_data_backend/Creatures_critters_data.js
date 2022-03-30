f7782501ebbae836c2cc702b81cb5d7301ac8877
Ravi Ranjan <raviranjan1140@gmail.com>
2022-03-30 21:41:10 +0530

    cresture and critters data all

---
+++ all_small_sliding_elements_data_backend/Creatures_critters_data.js
@@ -0,0 +1,33 @@
+
+
+var Creatures_critters=[
+    {
+        movie_image:"https://is5-ssl.mzstatic.com/image/thumb/owNSUO3F109RC75YDPMY7A/738x416.webp",
+        
+    },
+    {
+     movie_image:"https://is2-ssl.mzstatic.com/image/thumb/2lfdtvo9bvduvVLrbcASow/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is3-ssl.mzstatic.com/image/thumb/6f5y-DYx9UvvcJ-gpqAufw/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is2-ssl.mzstatic.com/image/thumb/FEcg1yXIW7vfftSbhFKsQg/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is1-ssl.mzstatic.com/image/thumb/AS1GpWWwwGwHxrXD7Sny5A/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/_Kq-GgPdDxxqWtSZ9GbHug/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is3-ssl.mzstatic.com/image/thumb/cuTV_0CMBiqg34YfIP2s0Q/738x416.webp",
+     
+ },
+ 
+]
