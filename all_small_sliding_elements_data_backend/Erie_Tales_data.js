bc42bac7c4d374e90a0c977ae0dcd7296a318985
Ravi Ranjan <raviranjan1140@gmail.com>
2022-03-30 21:41:42 +0530

    eries tales all data js

---
+++ all_small_sliding_elements_data_backend/Erie_Tales_data.js
@@ -0,0 +1,34 @@
+
+
+var Erie_Tales=[
+    {
+        movie_image:"https://is2-ssl.mzstatic.com/image/thumb/6tJakyTEtS0vrgdc9POZpg/738x416.webp",
+        
+    },
+    {
+     movie_image:"https://is1-ssl.mzstatic.com/image/thumb/yXhlXOFkS9yJ1kMmOhfIbg/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is4-ssl.mzstatic.com/image/thumb/h8oGFN-jdPeHY8j3EM_WnQ/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/Cof8vC9vaCH-GYX6JltrQA/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is4-ssl.mzstatic.com/image/thumb/EjmCDwq97jEEe9RuKY7aNw/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is2-ssl.mzstatic.com/image/thumb/78-I7VenST4ztZYfwMf6AQ/738x416.webp",
+     
+ },
+ {
+     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/NHLksWLSZpTcIw36-n7vWA/738x416.webp",
+     
+ },
+ 
+
+]
