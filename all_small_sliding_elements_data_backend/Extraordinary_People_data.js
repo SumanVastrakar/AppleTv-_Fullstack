801e10f33232beb8fac783a34741a35597f32ed6
Ravi Ranjan <raviranjan1140@gmail.com>
2022-03-30 21:42:52 +0530

    extra ordinary people data

---
+++ all_small_sliding_elements_data_backend/Extraordinary_People_data.js
@@ -0,0 +1,54 @@
+
+
+var Extraordinary_People=[
+    {
+        movie_image:"https://is4-ssl.mzstatic.com/image/thumb/qth_eNoJqMWbjMUyn16jpA/1478x832.webp",
+        movie_categories:"DOCUMENTARY",
+        movie_title:"Becoming You",
+        movie_description:"Olivia Colman narrates this docuseries about how kids grow in their first five years.",
+    },
+    {
+     movie_image:"https://is2-ssl.mzstatic.com/image/thumb/qAk-30MspnyIG9hVKJ1qKQ/1478x832.webp",
+     movie_categories:"DOCUMENTARY",
+     movie_title:"Dear...",
+     movie_description:"Profiles of 10 game-changing icons and those whose lives they’ve inspired.",
+ },
+ {
+     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/uBU112xIP48X2tW5EbpGGQ/1478x832.webp",
+     movie_categories:"DOCUMENTARY",
+     movie_title:"The Me You Can't See",
+     movie_description:"Oprah and Prince Harry guide honest discussions about mental health.",
+ },
+ {
+     movie_image:"https://is1-ssl.mzstatic.com/image/thumb/1hOcr3gchtyju9nzwvFcAg/1478x832.webp",
+     movie_categories:"DOCUMENTARY",
+     movie_title:"Home",
+     movie_description:"Discover the stories and ideas behind the world’s most imaginative dwellings.",
+ },
+ {
+     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/RMn-3IiFm6XMOJZT3tP8fA/1478x832.webp",
+     movie_categories:"DOCUMENTARY",
+     movie_title:"Visible: Out on Television",
+     movie_description:" Explore the history of the LGBTQ movement through the lens of TV.",
+ },
+ {
+     movie_image:"https://is2-ssl.mzstatic.com/image/thumb/jER5ceAuuLVFije5LmLVLw/1478x832.webp",
+     movie_categories:"DOCUMENTARY",
+     movie_title:"Greatness Code",
+     movie_description:"Seven iconic athletes reveal the pivotal career moment they touched greatness.",
+ },
+ {
+     movie_image:"https://is3-ssl.mzstatic.com/image/thumb/6MFkEmAhzwoAZfThq0xWHA/1478x832.webp",
+     movie_categories:"TALK SHOW",
+     movie_title:"The Oprah Conversation",
+     movie_description:"Intimate discussions with newsmakers, thought leaders, and masters of craft.",
+ },
+ {
+     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/QYnchKIdrKG_b0lkikPUSw/1478x832.webp",
+     movie_categories:"DOCUMENTARY",
+     movie_title:"Dads",
+     movie_description:"Bryce Dallas Howard's doc is a joyful, star-studded look at being a dad today.",
+ },
+ 
+ 
+ ]
