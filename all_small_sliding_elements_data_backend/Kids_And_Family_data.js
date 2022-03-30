b5d2230243f3ae10895a322c7a7162901ef799ee
Ravi Ranjan <raviranjan1140@gmail.com>
2022-03-30 22:40:14 +0530

    double quotes change and comma remove

--- all_small_sliding_elements_data_backend/Kids_And_Family_data.js
+++ all_small_sliding_elements_data_backend/Kids_And_Family_data.js
@@ -2,23 +2,23 @@
 
 var Kids_And_Family=[
     {
-        movie_image:"hhttps://is1-ssl.mzstatic.com/image/thumb/Features126/v4/95/66/71/95667123-58b3-212e-06b7-4c6fe10ee787/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfMS5sc3I.lsr/1478x832fe.webp",
+        "movie_image":"hhttps://is1-ssl.mzstatic.com/image/thumb/Features126/v4/95/66/71/95667123-58b3-212e-06b7-4c6fe10ee787/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfMS5sc3I.lsr/1478x832fe.webp"
         
     },
     {
-     movie_image:"https://is2-ssl.mzstatic.com/image/thumb/Features116/v4/40/e4/3a/40e43a6a-fa61-07b3-a77a-11c622f938fd/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfMi5sc3I.lsr/1478x832fe.webp",
+     "movie_image":"https://is2-ssl.mzstatic.com/image/thumb/Features116/v4/40/e4/3a/40e43a6a-fa61-07b3-a77a-11c622f938fd/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfMi5sc3I.lsr/1478x832fe.webp"
      
  },
  {
-     movie_image:"https://is1-ssl.mzstatic.com/image/thumb/Features116/v4/e7/7f/6d/e77f6d2a-f6c5-8c95-0d06-89c7e6edb080/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfMy5sc3I.lsr/1478x832fe.webp",
+     "movie_image":"https://is1-ssl.mzstatic.com/image/thumb/Features116/v4/e7/7f/6d/e77f6d2a-f6c5-8c95-0d06-89c7e6edb080/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfMy5sc3I.lsr/1478x832fe.webp"
      
  },
  {
-     movie_image:"https://is4-ssl.mzstatic.com/image/thumb/Features116/v4/f9/81/8f/f9818faf-8b61-a76a-3398-acf53238cacf/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfNC5sc3I.lsr/1478x832fe.webp",
+     "movie_image":"https://is4-ssl.mzstatic.com/image/thumb/Features116/v4/f9/81/8f/f9818faf-8b61-a76a-3398-acf53238cacf/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfNC5sc3I.lsr/1478x832fe.webp"
      
  },
  {
-     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/Features126/v4/e2/14/9a/e2149a62-3520-2f41-c465-04790607811c/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfNS5sc3I.lsr/1478x832fe.webp",
+     "movie_image":"https://is5-ssl.mzstatic.com/image/thumb/Features126/v4/e2/14/9a/e2149a62-3520-2f41-c465-04790607811c/U0YtVFZBLVVTQS1BVFZQbHVzX0tpZHMtQnJpY2tfNS5sc3I.lsr/1478x832fe.webp"
      
  },
 
--- all_small_sliding_elements_data_backend/Stories_From_Around_The_Globe_data.js
+++ all_small_sliding_elements_data_backend/Stories_From_Around_The_Globe_data.js
@@ -2,35 +2,35 @@
 
 var Stories_From_Around_The_Globe=[
     {
-        movie_image:"https://is1-ssl.mzstatic.com/image/thumb/fwOVLEMoZhr-wkppMR12XA/738x416.webp",
+        "movie_image":"https://is1-ssl.mzstatic.com/image/thumb/fwOVLEMoZhr-wkppMR12XA/738x416.webp"
         
     },
     {
-     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/KS0pkmrVEdO4uYZA5CpRZA/738x416.webp",
+     "movie_image":"https://is5-ssl.mzstatic.com/image/thumb/KS0pkmrVEdO4uYZA5CpRZA/738x416.webp"
      
  },
  {
-     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/y-GzfKyC0y6x5Dd8254GpQ/738x416.webp",
+     "movie_image":"https://is5-ssl.mzstatic.com/image/thumb/y-GzfKyC0y6x5Dd8254GpQ/738x416.webp"
      
  },
  {
-     movie_image:"https://is5-ssl.mzstatic.com/image/thumb/HSPjQEW5mDqwAbMbbhfNOQ/738x416.webp",
+     "movie_image":"https://is5-ssl.mzstatic.com/image/thumb/HSPjQEW5mDqwAbMbbhfNOQ/738x416.webp"
      
  },
  {
-     movie_image:"https://is3-ssl.mzstatic.com/image/thumb/kVTKGnVvCdFlB1sZeKa8YQ/738x416.webp",
+     "movie_image":"https://is3-ssl.mzstatic.com/image/thumb/kVTKGnVvCdFlB1sZeKa8YQ/738x416.webp"
      
  },
  {
-     movie_image:"https://is1-ssl.mzstatic.com/image/thumb/yXhlXOFkS9yJ1kMmOhfIbg/738x416.webp",
+     "movie_image":"https://is1-ssl.mzstatic.com/image/thumb/yXhlXOFkS9yJ1kMmOhfIbg/738x416.webp"
      
  },
  {
-     movie_image:"https://is4-ssl.mzstatic.com/image/thumb/rZ34KsuVSjdutVP-0HO95A/738x416.webp",
+     "movie_image":"https://is4-ssl.mzstatic.com/image/thumb/rZ34KsuVSjdutVP-0HO95A/738x416.webp"
      
  },
  {
-    movie_image:"https://is4-ssl.mzstatic.com/image/thumb/EjmCDwq97jEEe9RuKY7aNw/738x416.webp",
+    "movie_image":"https://is4-ssl.mzstatic.com/image/thumb/EjmCDwq97jEEe9RuKY7aNw/738x416.webp"
     
 },
  
