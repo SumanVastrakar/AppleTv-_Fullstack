0fa716ff91da3c9fb91125074fd822ed56c42a12
SumanVastrakar <96719499+SumanVastrakar@users.noreply.github.com>
2022-04-01 15:45:18 +0530

    Delete server.js

--- Apple Tv backend/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./index.js")
-const connect = require("./configs/db")
-app.listen (5000, async function(){
-    await connect ();
-    console.log("Listening on port 5000");
-})
